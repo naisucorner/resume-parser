5ab39dc6d20073fff78ebdde1cae9f327c66f523
Shion <naisucorner@gmail.com>
2022-10-12 12:01:46 +0700

    Test

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,11 +4,11 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 const App = ({ Component, pageProps }) => (
-  <>
+  <div>
     <Component {...pageProps} />
 
     <ToastContainer />
-  </>
+  </div>
 );
 
 export default App;
